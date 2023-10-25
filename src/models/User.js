ca33cd5f3d7c6044e6ca971aec751bfc4ef03214
Zafran Khan <96617852+Zafran002@users.noreply.github.com>
2023-10-25 16:55:17 +0530

    Update User.js model

--- src/models/User.js
+++ src/models/User.js
@@ -1,32 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const UserSchema = new Schema({
-
-    username: {
-        type: String,
+const UserSchema = new Schema(
+  {
+    profile: { type: String, default: null },
+    username: { type: String, required: true, unique: true },
+    user_type: {
+      type: String,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     otp: {
-        type: String,
+      type: String,
     },
     verification: {
-        type: Boolean,
+      type: Boolean,
     },
-    date: {
-        type: Date,
-        default: Date.now
+    token: {
+      type: String,
+      default: "",
     },
+    // addedBy: {
+    //     type: String,
+    // },
+    // addedBy_id: {
+    //     type: String,
+    // },
+    // is_active: {
+    //     type: Boolean,
+    // },
+    // isAdmin: {
+    //     type: Boolean,
+    //     default: false,
+    // },
 
     buff: Buffer,
-
-
-});
-module.exports = mongoose.model('user', UserSchema);
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("user", UserSchema);
